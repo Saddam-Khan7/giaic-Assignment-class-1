25855a5d7c453eb725b1a89f69765de1c8c49e18
Saddam-Khan7 <saddam64672@gmail.com>
2024-11-20 02:51:44 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <div className="h-screen w-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 flex items-center justify-center">
+    <h1 className="text-white text-4xl font-bold hover:cursor-pointer">Hello World</h1>
+
+    <h1 className="absolute top-5 left-5 text-2xl text-white">saddam khan</h1>
+  </div>  
+
+  
   );
 }
